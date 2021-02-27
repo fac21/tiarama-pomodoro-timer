63f6dee024da3a082cbfa1858b5fa4637a5dfc0a
Craig MacLeod <c92macleod@gmail.com>
2021-02-27 12:03:03 +0100

    Add styling of input tags when disabled

--- script.js
+++ script.js
@@ -60,8 +60,8 @@ function playPause() {
             currentWorkSecs = workMins*60
             currentBreakSecs = breakMins*60
             initial = false
-            worktimeInput.disabled = "true"
-            breaktimeInput.disabled = "true"
+            worktimeInput.disabled = true
+            breaktimeInput.disabled = true
         }
         if (isPaused) {
             isPaused = false
@@ -80,8 +80,8 @@ function reset() {
     const pauseIcon = document.getElementById("pause-icon")
     playIcon.style.display = "block"
     pauseIcon.style.display = "none"
-    worktimeInput.disabled = ""
-    breaktimeInput.disabled = ""
+    worktimeInput.disabled = false
+    breaktimeInput.disabled = false
     worktimeInput.value = ""
     breaktimeInput.value = ""
     initial = true
